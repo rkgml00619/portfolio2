718022470f1e3597c30bbb8ca713e93c6ea6a14d
한가희 <rkgml00619@naver.com>
2023-06-02 18:18:39 +0900

    두번째 포트폴리오

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 5000
+
+app.set("view engine","ejs")
+app.use(express.static('public'))
+
+app.use(express.json()) 
+app.use(express.urlencoded({ extended: true })) 
+
+app.listen(port,function(){
+  console.log("서버연결 성공");
+});
+
+app.get('/', (req, res) => {
+  res.render("index.ejs")
+})
